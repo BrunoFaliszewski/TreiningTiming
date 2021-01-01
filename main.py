67fb887744d499b456053ac8930e83433dbd2188
BrunoFaliszewski <bruno0faliszewski@gmail.com>
2021-01-01 19:50:27 +0100

    update

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
 from kivy.app import App
+from kivy.uix.popup import Popup
+from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
+from glob import glob
+from os import listdir
+from os.path import isfile, join
+import os
+import shutil
 
 Times = [5, 4, 3, 2, 1]
 Names = ["Start"]
+Sets = glob(os.path.join('Sets', "*", ""))
+for i in range(len(Sets)):
+    Sets[i] = Sets[i].replace('Sets', '')
+    Sets[i] = Sets[i].replace('\\', '')
+CurrentSet = ''
+TrainingText = ''
+print(Sets)
+
+class SavePopupWindow(Popup):
+    popuptextinput = ObjectProperty(None)
+
+    def PopupSavePressed(self):
+        global Sets, Times, Names, TrainingText
+        if len(Sets) < 9:
+            try:
+                Sets.append(self.popuptextinput.text)
+
+                os.mkdir(f'Sets\{self.popuptextinput.text}')
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}.txt', 'w') as self.file:
+                    for i in range(len(Times)):
+                        self.file.write(f'{Times[i]}\n')
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}.txt') as self.f_input:
+                    self.data = self.f_input.read().rstrip('\n')
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}.txt', 'w') as self.f_output:    
+                    self.f_output.write(self.data)
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}names.txt', 'w') as self.file:
+                    for i in range(len(Names)):
+                        self.file.write(f'{Names[i]}\n')
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}names.txt') as self.f_input:
+                    self.data = self.f_input.read().rstrip('\n')
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}names.txt', 'w') as self.f_output:    
+                    self.f_output.write(self.data)
+
+                with open(f'Sets\{self.popuptextinput.text}\{self.popuptextinput.text}trainingtext.txt', 'w') as self.file:
+                    self.file.write(TrainingText)
+
+                self.dismiss()
+            except FileExistsError:
+                self.popuptextinput.text = "There already is that set"
+        else:
+            self.popuptextinput.text = "You already have 9 Sets"
 
 class MainWindow(Screen):
     sets = ObjectProperty(None)
@@ -21,7 +75,7 @@ class MainWindow(Screen):
         Window.clearcolor = (33/255, 172/255, 47/255, 1)
 
     def AddPressed(self):
-        global Times, Names, Colors
+        global Times, Names, Colors, TrainingText
         if int(self.sets.text) > 0 and (int(self.workMinutes.text) * 60 + int(self.workSeconds.text) > 0 or int(self.restMinutes.text) * 60 + int(self.restSeconds.text) > 0):
             for i in range(int(self.sets.text)):
                 for j in range(int(self.workMinutes.text) * 60 + int(self.workSeconds.text)):
@@ -35,6 +89,8 @@ class MainWindow(Screen):
                 if int(self.restMinutes.text) * 60 + int(self.restSeconds.text) > 0:
                     Names.append("Rest")
             self.timesLabel += f"{self.sets.text}x Work: {self.workMinutes.text}:{self.workSeconds.text} Rest: {self.restMinutes.text}:{self.restSeconds.text}\n"
+            TrainingText = self.timesLabel
+            print(TrainingText)
     
     def ResetPressed(self):
         global Times
@@ -42,7 +98,8 @@ class MainWindow(Screen):
         self.timesLabel = ""
 
     def SavePressed(self):
-        pass
+        self.popup = SavePopupWindow()
+        self.popup.open()
 
 class TrainingWindow(Screen):
     seconds = StringProperty()
@@ -96,8 +153,56 @@ class TrainingWindow(Screen):
     def BackPressed(self):
         Clock.unschedule(self.schedule)
 
+class SetPopupWindow(Popup):
+    currentset = StringProperty()
+    timesLabel = StringProperty()
+
+    def on_open(self):
+        global CurrentSet
+        self.currentset = CurrentSet
+        with open(f'Sets\{CurrentSet}\{CurrentSet}trainingtext.txt', 'r') as self.file:
+            self.timesLabel = self.file.read()
+    
+    def RemovePressed(self):
+        global CurrentSet, Sets
+        shutil.rmtree(f'Sets\{CurrentSet}')
+        Sets.remove(f'{CurrentSet}')
+        self.dismiss()
+    
+    def StartPressed(self):
+        global Times, Names
+        Times = []
+        Names = []
+        with open(f'Sets\{CurrentSet}\{CurrentSet}.txt', 'r') as self.file:
+            self.line = self.file.readline()
+            while self.line:
+                Times.append(int(self.line.strip('\n')))
+                self.line = self.file.readline()
+        with open(f'Sets\{CurrentSet}\{CurrentSet}names.txt', 'r') as self.file:
+            self.line = self.file.readline()
+            while self.line:
+                Names.append(self.line.strip('\n'))
+                self.line = self.file.readline()
+        self.dismiss()
+        print(Times)
+        print(Names)
+
 class MySetsWindow(Screen):
-    pass
+    setsnames = ListProperty([])
+    currentset = StringProperty()
+
+    def on_pre_enter(self):
+        Clock.schedule_interval(self.update, 1)
+    
+    def update(self, dt):
+        global Sets
+        self.setsnames = Sets
+    
+    def SetPressed(self, instance):
+        global CurrentSet
+        CurrentSet = instance.text
+        self.popup = SetPopupWindow()
+        self.popup.open()
 
 class WindowManager(ScreenManager):
     pass
